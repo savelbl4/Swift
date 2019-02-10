bc89757d0e721b3b12338a371a3c61bd03c1145a
save_bl4 <savelbl4@gmail.com>
2019-02-10 13:02:44 +0300

    задание к четвёртому уроку

---
+++ 4L_Savelov Sergey/4L_Savelov Sergey/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  4L_Savelov Sergey
+//
+//  Created by serge on 10/02/2019.
+//  Copyright © 2019 serge. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
