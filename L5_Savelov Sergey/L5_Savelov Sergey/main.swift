47bdabfd71c1ad73d1916b29dcf5aff440f613fc
save_bl4 <savelbl4@gmail.com>
2019-02-14 12:40:25 +0300

    задание к пятому уроку

---
+++ L5_Savelov Sergey/L5_Savelov Sergey/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L5_Savelov Sergey
+//
+//  Created by serge on 14/02/2019.
+//  Copyright © 2019 serge. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
